0f1095dab424bd6e10f9a83042c14bbbe7a9385b
EZambranoFx <86860458+EZambranoFx@users.noreply.github.com>
2022-06-09 23:14:57 -0500

    DIP de pago

--- TallerSOLID/src/main/java/modelo/PagoPaypalO.java
+++ TallerSOLID/src/main/java/modelo/PagoPaypalO.java
@@ -1,15 +1,29 @@
 
 package modelo;
 
-public class PagoPaypalO extends PagoO{
+public class PagoPaypalO implements Pagable{
     private boolean loggedIn;   //Conexion a cuenta PayPal
     
-    @Override
     public void realizarCobro(double monto){
         if(!loggedIn)
             return;
         
         //Carga el monto de la compra al medio de pago
+    }
+
+    @Override
+    public void calcularImpuestosFactura() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void generarFactura() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void realizarCobro() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     
