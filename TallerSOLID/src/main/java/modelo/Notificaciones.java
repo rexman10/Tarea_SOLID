dada7f388f403222a8ce3058c724b5c985d70f03
ioriss <106688415+ioriss@users.noreply.github.com>
2022-06-09 23:40:31 -0500

    Add files via upload

---
+++ TallerSOLID/src/main/java/modelo/Notificaciones.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Notifica;
+
+/**
+ *
+ * @author jaime
+ */
+public class Notificaciones implements Notificacion{
+    
+    private int notificacion;
+
+    public Notificaciones(int notificacion) {
+        this.notificacion = notificacion;
+    }
+    
+    public void Notificar(Telegram telegram){}
+    
+}
